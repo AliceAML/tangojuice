fee9533b40df54fe56b039dd022852927dada1d7
alice.hammel <alice.hammel@gmail.com>
2021-11-08 19:02:06 +0100

    added imports back

--- get_vocab.py
+++ get_vocab.py
@@ -1,5 +1,5 @@
 # coding = utf8
-'''
+"""
 This module implements vocabulary extraction. It is able to perform the extraction from an URL to an article or a video from
 anyone of the following domain name:
 cn.nytimes.com (chinese new-york times)
@@ -8,11 +8,21 @@ token level procesing has yet to be implemented
 .. (to be continued: youtube,)
 Still very slow
 
-'''
+"""
+
+import json
+from collections import Counter
+import requests
+import lxml
+from bs4 import BeautifulSoup
 
 global threshold
 global punctuation
-punctuation = '1２3456789」(.·—，2＿≒＄（。＊１＋＾’！ー？『)＃９％』”＠、＆８３azertyuiopqsdfghjklmwxcvbnnAZERTYUIOPQSDFGHJKLMWXCVBN#*€Ùùéèàç!' + "'" + '"'
+punctuation = (
+    "1２3456789」(.·—，2＿≒＄（。＊１＋＾’！ー？『)＃９％』”＠、＆８３azertyuiopqsdfghjklmwxcvbnnAZERTYUIOPQSDFGHJKLMWXCVBN#*€Ùùéèàç!"
+    + "'"
+    + '"'
+)
 
 threshold = 5
 
@@ -23,7 +33,7 @@ with open("hskHanziList.json", "r") as istream:
 
 def squeeze_all_from_url(url):
     html = requests.get(url)
-    #insert a try except
+    # insert a try except
     soup = BeautifulSoup(html.text, "lxml")
     # extracting the raw paragraph while making sure we are note extracting the images captions
     raw_text = "\n".join(
@@ -40,7 +50,11 @@ def squeeze_all_from_url(url):
     print("spacy processing:\n")
     doc = nlp(raw_text)
     print("type doc:\n")
-    hanzi_list = [hanzi for hanzi in hanzi_list if get_hsk_level(hanzi) > threshold and hanzi not in punctuation]
+    hanzi_list = [
+        hanzi
+        for hanzi in hanzi_list
+        if get_hsk_level(hanzi) > threshold and hanzi not in punctuation
+    ]
     # Add an option: if text is too long refuse it
     # if the proportion of level above threshold is too high, tell the user to choose an easier text
     return infos_and_levels(hanzi_list)
@@ -89,7 +103,6 @@ def infos_and_levels(raw_text):
 
 
 if __name__ == "__main__":
-    #test_text = "子列居鄭圃，四十年人无識者。乎？」子列子笑曰：「壺子何言哉？雖然，夫子嘗語伯昏瞀人，吾側聞之，試以告女。其言曰：有生不生，有化不化。不生者能生生，不化者能化化。生者不能不生，化者不能不化，故常生常化。常生常化者，无時不生，无時不化。陰陽爾，四時爾，不生者疑獨，不化者往復。往復，其際不可終；疑獨，其道不可窮。《黃帝書》曰：「谷神不死，是謂玄牝。玄牝之門，是謂天地之根。綿綿若存，用之不勤。」故生物者不生，化物者不化。自生自化，自形自色，自智自力，自消自息。謂之生化、形色、智力、消息者，非也。"
+    # test_text = "子列居鄭圃，四十年人无識者。乎？」子列子笑曰：「壺子何言哉？雖然，夫子嘗語伯昏瞀人，吾側聞之，試以告女。其言曰：有生不生，有化不化。不生者能生生，不化者能化化。生者不能不生，化者不能不化，故常生常化。常生常化者，无時不生，无時不化。陰陽爾，四時爾，不生者疑獨，不化者往復。往復，其際不可終；疑獨，其道不可窮。《黃帝書》曰：「谷神不死，是謂玄牝。玄牝之門，是謂天地之根。綿綿若存，用之不勤。」故生物者不生，化物者不化。自生自化，自形自色，自智自力，自消自息。謂之生化、形色、智力、消息者，非也。"
     article_url = "https://cn.nytimes.com/business/20211029/china-coal-climate/"
     char_info, occurencies, levels = squeeze_all_from_url(article_url)
-
