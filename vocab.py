ee2a9c1bd928d41afe8a45ff6644afafc51e9657
alice.hammel <alice.hammel@gmail.com>
2021-11-09 09:46:27 +0100

    fixed mistakes. Rejected tokens from closed class words

--- vocab.py
+++ vocab.py
@@ -7,8 +7,19 @@ import spacy
 
 from scraper import scrape
 
+# les "fréquences relatives" sont calculées par rapport au mot le plus fréquent
+# car on n'a pas le nb de tokens du corpus utilisé
+# c'est sûrement pas top !
+
+# TODO déterminer seuils !!!
+# peuvent varier en fonction des langues
+STOPWORD_THRESHOLD = 0.2
+RARE_WORD_THRESHOLD = 0.1e-2
+
+OPEN_CLASS_WORDS = ["ADJ", "ADV", "INTJ", "NOUN", "PROPN", "VERB"]
+
 MODELS = {
-    "de": "de_core_news_sm",
+    "de": "de_core_news_sm",  # "de_dep_news_trf",
     "en": "en_core_web_sm",
     "fr": "fr_core_news_sm",
     "ja": "ja_core_news_sm",
@@ -30,9 +41,8 @@ class Word:
         self.lemme = lemme
         self.pos = pos
         self.occurrences = []
-        self.lang_freq = lang_frequencies[
-            lemme
-        ]  # FIXME c'est un problème de prendre seulement la fréquence du lemme...
+        self.lang_freq = lang_frequencies.get(lemme, 0)
+        # FIXME c'est un problème de prendre seulement la fréquence du lemme...
         self.doc_freq = 0
 
     def add_occurrence(self, sentence):
@@ -42,14 +52,14 @@ class Word:
         self.occurrences.append(sentence)
         self.doc_freq += 1
 
-    def is_rare() -> bool:
+    def is_rare(self) -> bool:
         """
         Return true if this word is rare
         """
-        return False  # TODO déterminer un seuil...
+        return self.lang_freq < RARE_WORD_THRESHOLD
 
     def __str__(self):
-        return f"{self.id} {self.lemme.upper()} {self.pos} {self.doc_freq} {self.lang_freq}"
+        return f"{self.id:<4} {self.lemme:<15} {self.pos:<5} {self.doc_freq:<2} {self.lang_freq:.10f}"  # {self.occurrences}"
 
 
 class Vocabulary:
@@ -64,22 +74,29 @@ class Vocabulary:
         """
         Add tokens in the sentence to the vocabulary.
         """
+        # print(sentence)
         for word in sentence:
-            # do not include stopwords  > TODO déterminer seuil !!!
-            if self.lang_frequencies.get(word.lemma_, 1) > 0.05:
+            key = word.lemma_ + word.pos_
+            # print(key)
+            # do not include stopwords and punctuation
+            if (
+                self.lang_frequencies.get(word.lemma_, 0) > STOPWORD_THRESHOLD
+                # or not word.is_alpha
+                # TODO rejecter toutes les POS fermées ?
+                or word.pos_ not in OPEN_CLASS_WORDS
+                # complete list : https://universaldependencies.org/u/pos/
+            ):
                 continue
-            elif word.lemma_ in self.words:
-                self.words[word.lemma_].add_occurrence(sentence)
+            elif key in self.words:
+                self.words[key].add_occurrence(sentence.text)
             else:
-                self.words[word.lemma_] = Word(
-                    word.lemma_, word.pos_, self.lang_frequencies
-                )
-                self.words[word.lemma_].add_occurrence(sentence)
+                self.words[key] = Word(word.lemma_, word.pos_, self.lang_frequencies)
+                self.words[key].add_occurrence(sentence.text)
 
     def __str__(self) -> str:
         return "\n".join(str(word) for word in self.words.values() if word.doc_freq > 1)
 
-    def select_vocab(self, nb_words=None, includeCommonWords: bool = True):
+    def extract_vocab(self, nb_words=None, onlyRareWords: bool = False):
         """
         Return a list of nb_words most frequent words.
         """
@@ -87,16 +104,14 @@ class Vocabulary:
         if nb_words is None:
             nb_words = len(self.words)  # default value = return all the words
 
-        if includeCommonWords:
-            return sorted(
-                self.words.values(), key=lambda word: word.doc_freq, reverse=True
-            )[:nb_words]
+        if onlyRareWords:
+            word_list = (word for word in self.words.values() if word.is_rare())
         else:
-            return sorted(
-                (word for word in self.words.values() if word.is_rare()),
-                key=lambda word: word.doc_freq,
-                reverse=True,
-            )[:nb_words]
+            word_list = self.words.values()
+
+        return sorted(word_list, key=lambda word: word.doc_freq, reverse=True)[
+            :nb_words
+        ]
 
 
 def make_vocab(text, lang):
@@ -104,7 +119,7 @@ def make_vocab(text, lang):
     lang_frequencies = json.load(open(f"frequency_lists/{lang}_full.json", "r"))
     vocab = Vocabulary(lang_frequencies)
 
-    print(f"Load {lang} SpaCy model")
+    print(f"Load {MODELS[lang]} SpaCy model")
     nlp = spacy.load(MODELS[lang])
     print("Parse text")
     doc = nlp(text)
@@ -116,13 +131,13 @@ def make_vocab(text, lang):
 
 
 if __name__ == "__main__":
-    LANG = "fr"
+    LANG = "de"
     text = scrape(
-        "https://u-paris.fr/linguistique/procedure-pour-effectuer-un-stage/",
+        "https://www.tagesschau.de/inland/ampelparteien-reaktionen-101.html",
         lang=LANG,
     )
     vocab = make_vocab(text, lang=LANG)
-    selected_vocab = vocab.select_vocab(nb_words=20)
+    selected_vocab = vocab.extract_vocab(nb_words=20, onlyRareWords=False)
     for word in selected_vocab:
         print(word)
 
