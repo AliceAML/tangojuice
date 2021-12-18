46c1b84f7aa97f6a70ac63100a3c2d1828c2bdfd
alice.hammel <alice.hammel@gmail.com>
2021-12-18 20:16:23 +0100

    now calculating frequencies for lemmas (super long to do)

--- frequency_lists/freq_to_json.py
+++ frequency_lists/freq_to_json.py
@@ -1,29 +1,54 @@
 import json
 import sys
 import math
+import spacy
+from collections import defaultdict
+from tqdm import tqdm
+
+MODEL_NAMES = {
+    "de": "de_core_news_sm",  # "de_dep_news_trf",
+    "en": "en_core_web_sm",
+    "fr": "fr_core_news_sm",
+    "ja": "ja_core_news_sm",
+    "no": "nb_core_news_sm",
+    "zh": "zh_core_web_sm",
+}
 
 
 def convert_freq_list_to_json(file):
     """
     Converts a frequency list to a json file.
     """
+    lang = file.split("_")[0]
+    print(f"Load {MODEL_NAMES[lang]}")
+    nlp = spacy.load(MODEL_NAMES[lang])
 
-    freqs = {}
     with open(file, "r") as f:
         total = 0
+        nb_lines = 0
         for line in f:
-            total += int(line.split()[1])
+            nb_lines += 1
+            form, freq = line.split()
+            total += int(freq)
 
     print(f"{total} total words")
-
+    print(f"{nb_lines} lines")
+    freqs = {}
+    freqs_lemmas = defaultdict(float)
     with open(file, "r") as f:
-        for line in f:
+        for line in tqdm(f, total=nb_lines):
             word, count = line.split()
             count = int(count)
             freqs[word] = int(count) / total
 
+            lemma = nlp(word)[0].lemma_
+            freqs_lemmas[lemma] += int(count) / total
+
     with open(file.replace(".txt", ".json"), "w") as g:
         json.dump(freqs, g, indent=2)
+
+    with open(file.replace(".txt", "_lemmas.json"), "w") as g:
+        json.dump(freqs_lemmas, g, indent=2)
 
 
 print(f"Convert {sys.argv[1]}")
